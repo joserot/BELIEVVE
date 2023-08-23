b7f8ae22240f01f821a29fb56503418d591a678d
joserot <jose.rotchen14@gmail.com>
2023-08-23 14:59:08 -0300

    cambios en pagina main

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,22 @@
 import styles from "./page.module.css";
 import Header from "<src>/components/common/Header/Header";
 import HeroHome from "<src>/components/home/Hero/Hero";
+import Information from "<src>/components/home/Information/Information";
+
+const orlando = {
+	name: "Orlando",
+	description: "Family Fun in the Heart of the Parks",
+	imagePrimary: "",
+	imageSecondary: "",
+	imageTertiary: "",
+};
 
 export default function Home() {
 	return (
-		<>
+		<main>
 			<Header />
 			<HeroHome />
-			<main className={styles.main}></main>
-		</>
+			<Information />
+		</main>
 	);
 }
---
+++ src/components/home/Information/Information.tsx
@@ -0,0 +1,14 @@
+import styles from "./Information.module.css";
+
+export default function Information() {
+	return (
+		<article className={styles.information}>
+			<h2>Ready, Set, Go!</h2>
+			<p>
+				Let us help you create unforgettable memories during your getaway. With
+				an array of destinations to choose from, we can make your dream vacation
+				a reality.
+			</p>
+		</article>
+	);
+}
