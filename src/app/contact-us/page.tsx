b78c3cd60802cf6dafdad12726227832df60bb45
Jose Rotchen <jose.rotchen14@gmail.com>
2023-09-14 08:12:33 -0300

    cambios en contact-us

--- src/app/contact-us/page.tsx
+++ src/app/contact-us/page.tsx
@@ -2,14 +2,16 @@ import Header from "<src>/components/common/Header/Header";
 import Footer from "<src>/components/common/Footer/Footer";
 import WhatsAppButton from "<src>/components/common/WhatsAppButton/WhatsAppButton";
 import Hero from "<src>/components/contact-us/Hero/Hero";
+import About from "<src>/components/contact-us/About/About";
 
 export default function contactUs() {
-	return (
-		<>
-			<Header />
-			<Hero />
-			<WhatsAppButton />
-			<Footer />
-		</>
-	);
+  return (
+    <>
+      <Header />
+      <Hero />
+      <About />
+      <WhatsAppButton />
+      <Footer />
+    </>
+  );
 }
--- src/components/destinations/Hotel/Hotel.tsx
+++ src/components/destinations/Hotel/Hotel.tsx
@@ -7,65 +7,65 @@ import "@splidejs/splide/css";
 import Image from "next/image";
 
 interface orientation {
-	orientation?: "left" | "right";
+  orientation?: "left" | "right";
 }
 
 type Props = Hotel & orientation;
 
 export default function Hotel({
-	title,
-	description,
-	longDescription,
-	parking,
-	images,
-	orientation = "left",
+  title,
+  description,
+  longDescription,
+  parking,
+  images,
+  orientation = "left",
 }: Props) {
-	return (
-		<article className={styles.hotel}>
-			<h2>{title}</h2>
+  return (
+    <article className={styles.hotel}>
+      <h2>{title}</h2>
 
-			<section
-				className={
-					orientation === "right"
-						? styles.hotelContainerRight
-						: styles.hotelContainerLeft
-				}
-			>
-				{images && images.length ? (
-					<div className={styles.hotelImages}>
-						<Splide
-							options={{
-								type: "loop",
-								perPage: 1,
-								arrows: true,
-								paginationKeyboard: true,
-								width: "100%",
-								height: "500px",
-								lazyLoad: true,
-							}}
-							aria-label="Images"
-						>
-							{images.map((image) => {
-								return (
-									<SplideSlide key={image}>
-										<Image
-											className={styles.image}
-											src={image}
-											alt={"Hotel"}
-											width={300}
-											height={300}
-										/>
-									</SplideSlide>
-								);
-							})}
-						</Splide>
-					</div>
-				) : null}
-				<div className={styles.description}>
-					<p>{description}</p>
-				</div>
-			</section>
-			<button className={styles.moreInformation}>MORE INFORMATION</button>
-		</article>
-	);
+      <section
+        className={
+          orientation === "right"
+            ? styles.hotelContainerRight
+            : styles.hotelContainerLeft
+        }
+      >
+        {images && images.length ? (
+          <div className={styles.hotelImages}>
+            <Splide
+              options={{
+                type: "loop",
+                perPage: 1,
+                arrows: true,
+                paginationKeyboard: true,
+                width: "100%",
+                height: "500px",
+                lazyLoad: true,
+              }}
+              aria-label="Images"
+            >
+              {images.map((image) => {
+                return (
+                  <SplideSlide key={image}>
+                    <Image
+                      className={styles.image}
+                      src={image}
+                      alt={"Hotel"}
+                      width={300}
+                      height={300}
+                    />
+                  </SplideSlide>
+                );
+              })}
+            </Splide>
+          </div>
+        ) : null}
+        <div className={styles.description}>
+          <p>{description}</p>
+        </div>
+      </section>
+      <button className={styles.moreInformation}>MORE INFORMATION</button>
+    </article>
+  );
 }
