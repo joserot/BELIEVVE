eb3dd6465de132600b288047b9e713721deec5c7
Jose Rotchen <jose.rotchen14@gmail.com>
2023-09-14 09:14:21 -0300

    pagina de contacto

--- src/app/contact-us/page.tsx
+++ src/app/contact-us/page.tsx
@@ -3,12 +3,14 @@ import Footer from "<src>/components/common/Footer/Footer";
 import WhatsAppButton from "<src>/components/common/WhatsAppButton/WhatsAppButton";
 import Hero from "<src>/components/contact-us/Hero/Hero";
 import About from "<src>/components/contact-us/About/About";
+import Contact from "<src>/components/contact-us/Contact/Contact";
 
 export default function contactUs() {
   return (
     <>
       <Header />
       <Hero />
+      <Contact />
       <About />
       <WhatsAppButton />
       <Footer />
---
+++ src/components/contact-us/Contact/Contact.tsx
@@ -0,0 +1,40 @@
+"use client";
+
+import styles from "./Contact.module.css";
+
+import Image from "next/image";
+import { useState } from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faPhone, faEnvelope } from "@fortawesome/free-solid-svg-icons";
+import { faWhatsapp } from "@fortawesome/free-brands-svg-icons";
+
+export default function Contact() {
+  const [show, setShow] = useState(false);
+
+  const handleShow = () => {
+    setShow(!show);
+  };
+
+  return (
+    <article className={styles.contact}>
+      <h2>Please choose the best way to contact us.</h2>
+
+      <div className={styles.container}>
+        <div className={show ? styles.linksOpen : styles.linksClose}>
+          <a className={styles.phone} href="!#" target="_blank">
+            <FontAwesomeIcon icon={faPhone} />
+          </a>
+          <a className={styles.whatsapp} href="!#" target="_blank">
+            <FontAwesomeIcon icon={faWhatsapp} />
+          </a>
+          <a className={styles.email} href="!#" target="_blank">
+            <FontAwesomeIcon icon={faEnvelope} />
+          </a>
+        </div>
+        <button onClick={handleShow} className={styles.buttonMain}>
+          <Image src={"/img/logo-2.png"} alt={"logo"} width={50} height={50} />
+        </button>
+      </div>
+    </article>
+  );
+}
