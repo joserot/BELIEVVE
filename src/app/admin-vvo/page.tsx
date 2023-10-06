5b9968b41d5f29d8aa9672f8e75a26944be6e08b
Jose Rotchen <jose.rotchen14@gmail.com>
2023-10-06 13:55:19 -0300

    error handler

--- src/app/admin-vvo/page.tsx
+++ src/app/admin-vvo/page.tsx
@@ -47,6 +47,7 @@ export default function PageAdmin() {
       }
     } catch (error) {
       console.log(error);
+      alert("Error uploading");
     } finally {
       setIsLoading(false);
     }
